1f73b8951ed8e847e1468623c50aed89ca2ad822
Demba <demba_ba@yahoo.fr>
2015-07-08 16:14:59 +0200

    css empty folder

---
+++ config.rb
@@ -0,0 +1,8 @@
+preferred_syntax = :sass
+http_path = '/'
+css_dir = 'views/stylesheets/css'
+sass_dir = 'views/stylesheets/sass'
+#images_dir = 'assets/images'
+#javascripts_dir = 'assets/javascripts'
+relative_assets = true
+line_comments = true
